938dce032ad9c116f62ee18b2f119fb858f9a221
Bogdan Petrovskiy <82778510+kefaxoo@users.noreply.github.com>
2023-03-19 22:28:51 +0300

    Add comments

--- ulam-spiral/untitled/src/Main.java
+++ ulam-spiral/untitled/src/Main.java
@@ -5,23 +5,23 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        try (var fis = new FileInputStream("result.bin")) {
-            byte[] buffer = new byte[256];
-            System.out.println("File data:");
+        try (var fis = new FileInputStream("result.bin")) { // здесь создаём объект потока чтения из файла
+            byte[] buffer = new byte[256]; // создаём массив байтов для чтения из файла
+            System.out.println("File data:"); // вывод сообщения о чтении данных из файла
 
-            int count;
-            while((count=fis.read(buffer))!=-1){
+            int count; // переменная для количества символов в файле
+            while((count=fis.read(buffer))!=-1){ // пока это количество не равно -1 (количество берётся из файла (максимально 256 байтов)
 
-                for(int i=0; i<count;i++){
+                for(int i=0; i<count;i++){ // выводим все эти байты
 
-                    System.out.print((char)buffer[i]);
+                    System.out.print((char)buffer[i]); // переводя в символы
                 }
             }
-        } catch (IOException exception) {
-            System.out.println(exception.getMessage());
+        } catch (IOException exception) { // если не получилось создать объект (в основном из-за отсутствия файла в директории)
+            System.out.println(exception.getMessage()); // выведем ошибку
         }
 
-        Frame frame = new Frame(600, true);
+        Frame frame = new Frame(600, true); // создание окна с спиралью, первый параметр - размер окна, второй - только ли простые числа в спирали
         Frame allNumbers = new Frame(600, false);
     }
 }
--- ulam-spiral/untitled/src/by/kefaxoo/frame/Frame.java
+++ ulam-spiral/untitled/src/by/kefaxoo/frame/Frame.java
@@ -6,114 +6,114 @@ import java.io.*;
 
 public class Frame extends javax.swing.JFrame {
 
-    private boolean isPrime = false;
-    private int previousX;
-    private int previousY;
+    private boolean isPrime = false; // свойство для показа только простых чисел
+    private int previousX; // координата предыдущей точки по х
+    private int previousY; // по у
 
     public Frame(int size, boolean isPrime) {
-        setSize(size, size);
-        setBackground(null);
-        setVisible(true);
-        setTitle("Ulam spiral");
-        setResizable(false);
-        this.isPrime = isPrime;
-        int x = size / 2 - 20;
+        setSize(size, size); // окно размера
+        setBackground(null); // установка фонового цвета окна
+        setVisible(true); // будет ли видно окно
+        setTitle("Ulam spiral"); // название окна
+        setResizable(false); // можно ли изменять размер окна
+        this.isPrime = isPrime; // сохраняем в свойство объекта информации о выводе только простых чисел
+        int x = size / 2 - 20; // вычисляем координаты первого значения по х
         previousX = x;
-        int y = size / 2 - 30;
+        int y = size / 2 - 30; // по у
         previousY = y;
-        int dx = 1;
-        int dy = 0;
-        int step = 1;
-        int turn = 1;
-        int number = 1;
-        double angle = 0.0;
-        int maxValue = 1;
-        while ((x > 0 && x < size) && (y > 0 && y < size)) {
-            if (number - 1 == turn) {
-                angle += 90;
-                if ((dx == 0 && dy == -1) || (dx == 0 && dy == 1)) {
-                    step++;
+        int dx = 1; // это переменная для изменения координаты по х для следующего числа
+        int dy = 0; // по у
+        int step = 1; // переменная для шага
+        int turn = 1; // для поворота
+        int number = 1; // для числа
+        double angle = 0.0; // для угла
+        int maxValue = 1; // переменная максимального числа
+        while ((x > 0 && x < size) && (y > 0 && y < size)) { // пока мы находимся в окне (по координатам)
+            if (number - 1 == turn) { // если вдруг пришёл момент для поворота
+                angle += 90; // добавляем к углу 90 градусов
+                if ((dx == 0 && dy == -1) || (dx == 0 && dy == 1)) { // если число находится, где х == константа
+                    step++; // + 1 значение к шагу
                 }
 
-                turn += step;
-                dx = (int)Math.cos(Math.toRadians(angle));
+                turn += step; // к повороту добавляем количество шагов
+                dx = (int)Math.cos(Math.toRadians(angle)); // вычисляем по какой координате двигаемся (будет или 0 или 1)
                 dy = (int)Math.sin(Math.toRadians(-angle));
             }
 
-            drawSpiral(number, x, y, angle);
-            previousX = x;
+            drawSpiral(number, x, y, angle); // вызываем функцию рисования цифр
+            previousX = x; // сохраняем в предыдущие координаты нарисованное число
             previousY = y;
-            x += dx * 80;
+            x += dx * 80; // вычисляем коордианты следующего числа
             y += dy * 80;
-            number += 1;
-            maxValue = number;
+            number += 1; // увеличиваем следующее число на +1
+            maxValue = number; // и сохраняем его
         }
 
-        var label = new JLabel("");
-        label.setBounds(-100, -100, 10, 10);
-        this.add(label);
+        var label = new JLabel(""); // создаём пустой лэйбл для "обхода" бага с магическим перемещением последнего элемента
+        label.setBounds(-100, -100, 10, 10); // делаем для него привязку
+        this.add(label); // и добавляем на экран
 
-        if (isPrime) {
-            var listOfNumbers = new ArrayList<ArrayList<Integer>>();
+        if (isPrime) { // затем если окно с простыми числами
+            var listOfNumbers = new ArrayList<ArrayList<Integer>>(); // создаём матрицу с простыми числами
 
-            for (int i = 2; i <= maxValue; i++) {
-                if (isPrimeNumber(i, 2)) {
-                    if (listOfNumbers.size() < decimalToBytes(i).length() / 8) {
-                        listOfNumbers.add(new ArrayList<Integer>());
+            for (int i = 2; i <= maxValue; i++) { // в которой проходимся от 2 до максимального числа
+                if (isPrimeNumber(i, 2)) { // если число в итерации цикла простое
+                    if (listOfNumbers.size() < decimalToBytes(i).length() / 8) { // проверяем его количество байтов, если строки для его количества байтов нет
+                        listOfNumbers.add(new ArrayList<Integer>()); // создаём новую строку
                     }
 
-                    listOfNumbers.get((decimalToBytes(i).length() / 8) - 1).add(i);
+                    listOfNumbers.get((decimalToBytes(i).length() / 8) - 1).add(i); // записываем в нужную строку число
                 }
             }
 
-            try (FileOutputStream fos = new FileOutputStream("result.bin")) {
-                StringBuilder line = new StringBuilder();
-                for (var numbers: listOfNumbers) {
-                    line.append(numbers.size() + " ");
-                    for (int i = 0; i < numbers.size(); i++) {
-                        line.append(numbers.get(i));
-                        if (i != numbers.size() - 1) {
-                            line.append(" ");
-                        } else {
-                            line.append('\n');
+            try (FileOutputStream fos = new FileOutputStream("result.bin")) { // затем мы создаём объект для записи в бинарный файл
+                StringBuilder line = new StringBuilder(); // создаём строку для записи
+                for (var numbers: listOfNumbers) { // проходимся по всей матрице
+                    line.append(numbers.size() + " "); // и первым значением в строке будет количество цифр в строке матрице
+                    for (int i = 0; i < numbers.size(); i++) { // затем мы проходимся по строке
+                        line.append(numbers.get(i)); // и добавляем каждое число
+                        if (i != numbers.size() - 1) { // если число не последнее
+                            line.append(" "); // добавим разделитель пробел
+                        } else { // иначе
+                            line.append('\n'); // перейдём на новую строку
                         }
                     }
 
-                    fos.write(String.valueOf(line).getBytes(), 0, String.valueOf(line).getBytes().length);
-                    line = new StringBuilder();
+                    fos.write(String.valueOf(line).getBytes(), 0, String.valueOf(line).getBytes().length); // а затем записываем в бинарный файл эту строку
+                    line = new StringBuilder(); // и очищаем строку, чтобы при следующей записи в ней не было уже записанных данных
                 }
-            } catch (IOException exception) {
-                System.out.println(exception.getMessage());
+            } catch (IOException exception) { // а если мы не смогли записать
+                System.out.println(exception.getMessage()); // будет ерор
             }
         }
     }
 
-    private boolean isPrimeNumber(int n, int step) {
-        if (step == n || n < 3) {
-            return true;
+    private boolean isPrimeNumber(int n, int step) { // рекурсивная функция на проверку является ли числом простым
+        if (step == n || n < 3) { // если step равен числу или число меньше трёх
+            return true; // оно простое
         }
 
-        if (n % step == 0) {
-            return false;
+        if (n % step == 0) { // если число делится на step без остатка
+            return false; // то оно не простое
         }
 
-        return isPrimeNumber(n, step + 1);
+        return isPrimeNumber(n, step + 1); // и вызовем снова эту функцию с шагом +1
     }
 
-    private String decimalToBytes(int decimalValue) {
-        var binaryString = new StringBuffer(Integer.toBinaryString(decimalValue));
-        while (binaryString.length() % 8 != 0) {
-            binaryString.insert(0, '0');
+    private String decimalToBytes(int decimalValue) { // функция перевода десятичного числа в байты
+        var binaryString = new StringBuffer(Integer.toBinaryString(decimalValue)); // строка с двоичным представлением десятичного числа
+        while (binaryString.length() % 8 != 0) { // и если его количество байт не кратно восьми
+            binaryString.insert(0, '0'); // будет до него добавлять нули
         }
 
-        return String.valueOf(binaryString);
+        return String.valueOf(binaryString); // и вернём число в байтовом представлении
     }
 
-    private void drawSpiral(int number, int x, int y, double angle) {
-        var numberLabel = new JLabel(String.valueOf(number));
-        if (isPrime) {
-            if (!isPrimeNumber(number, 2) || number == 1) {
-                if (angle % 180 == 0) {
+    private void drawSpiral(int number, int x, int y, double angle) { // функция рисования числа на спирали
+        var numberLabel = new JLabel(String.valueOf(number)); // лейбл с числом
+        if (isPrime) { // если сейчас спираль только с простыми числами
+            if (!isPrimeNumber(number, 2) || number == 1) { // и если это число не простое
+                if (angle % 180 == 0) { // проверяем какая палка будет вместо числа
                     numberLabel.setText("-");
                 } else {
                     numberLabel.setText("|");
@@ -121,28 +121,28 @@ public class Frame extends javax.swing.JFrame {
             }
         }
 
-        numberLabel.setBounds(x, y, 30, 30);
-        this.add(numberLabel);
+        numberLabel.setBounds(x, y, 30, 30); // выставляем лейблу расположение в окне
+        this.add(numberLabel); // и добавляем на экран это число/палочку
 
-        if (previousX != x || previousY != y) {
-            var lineLabel = new JLabel();
-            if (previousX == x) {
-                lineLabel.setText("|");
-                if (previousY - y < 0) {
-                    lineLabel.setBounds(x, previousY + 40, 20, 20);
+        if (previousX != x || previousY != y) { // если это не первое число
+            var lineLabel = new JLabel(); // создаём новый лабел на палочку между числами
+            if (previousX == x) { // если координаты чисел по х равны
+                lineLabel.setText("|"); // будет вертикальная палочка
+                if (previousY - y < 0) { // здесь вычисляется знак у дельта у, если дельта меньше нуля
+                    lineLabel.setBounds(x, previousY + 40, 20, 20); // то относительно предыдущего числа палочка будет ниже по у (хоть и здесь +)
                 } else {
-                    lineLabel.setBounds(x, previousY - 40, 20, 20);
+                    lineLabel.setBounds(x, previousY - 40, 20, 20); // ну или выше
                 }
-            } else if (previousY == y) {
+            } else if (previousY == y) { // the same, но если у равен
                 lineLabel.setText("-");
                 if (previousX - x < 0) {
-                    lineLabel.setBounds(previousX + 40, y, 20, 20);
+                    lineLabel.setBounds(previousX + 40, y, 20, 20); // здесь по-нормальному, то есть + вправо
                 } else {
-                    lineLabel.setBounds(previousX - 40, y, 20, 20);
+                    lineLabel.setBounds(previousX - 40, y, 20, 20); // а если -, то в лево
                 }
             }
 
-            this.add(lineLabel);
+            this.add(lineLabel); // ну и добавляем палочку
         }
     }
 }
